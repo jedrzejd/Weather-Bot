8839d178afca744236f5290148467ed669f77601
Jędrzej Dudzicz <jedrzejdudzicz@gmail.com>
2021-06-26 17:50:30 +0200

    Adding Weather condition

--- request.py
+++ request.py
@@ -7,7 +7,7 @@ from tokens import WEATHER_TOKEN
 def get_current_weather_from_city_name(city_name):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&lang=pl&appid={WEATHER_TOKEN}'
     data = json.loads(requests.get(url).content)
-    return parse_data(data)
+    return data
 
 
 def parse_data(data):
--- weather.py
+++ weather.py
@@ -4,15 +4,17 @@ from request import get_current_weather_from_city_name
 color = 0xFF6500
 default_city = 'Białystok'
 key_features = {
-    'temp': 'temperatura',
-    'feels_like': 'temperatura odczuwalna',
-    'pressure': 'ciśnienie',
-    'humidity': 'wilgotność'
+    'temp': 'Temperatura',
+    'feels_like': 'Temperatura odczuwalna',
+    'pressure': 'Ciśnienie',
+    'humidity': 'Wilgotność'
 }
 
 
 def weather_message(location=default_city):
     data = get_current_weather_from_city_name(location)
+    main_data = data['main']
+    additional_data = data['weather']
     message = discord.Embed(
         title=f'Pogoda {location}',
         description=f'Obecna pogoda w mieście {location}.',
@@ -21,9 +23,14 @@ def weather_message(location=default_city):
     for key in key_features:
         message.add_field(
             name=key_features[key],
-            value=str(data[key]),
+            value=str(main_data[key]),
             inline=False
         )
+    message.add_field(
+        name='Warunki pogodowe',
+        value=str(additional_data[0]['description']),
+        inline=False
+    )
     return message
 
 
